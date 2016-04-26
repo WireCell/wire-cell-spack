d21e7b475cea5b1e8ef11084622766fb9bdc862a
Gogs <gogs@fake.local>
2016-04-25 21:20:47 -0400

    Start the packaging for the toolkit itself.
    Not working yet.

--- wc-spack/packages/wirecell-toolkit/package.py
+++ wc-spack/packages/wirecell-toolkit/package.py
@@ -1,4 +1,4 @@
-from spack import Package, version, depends_on
+from spack import *
 import os
 class WirecellToolkit(Package):
     """Description"""
@@ -6,19 +6,28 @@ class WirecellToolkit(Package):
     homepage = "http://wirecell.github.io"
     #url = "http://wirecell.github.io"
 
-    version('dev', git="git@github.com:WireCell/wire-cell-build.git")
+    version('dev', git="https://github.com/WireCell/wire-cell-build.git")
 
     depends_on("eigen")
     depends_on("tbb")
-    depends_on("boost@1.60.0:")
+    depends_on("boost@1.59.0")
     depends_on("root@6:")
 
     def install(self, spec, prefix):
+        bash = which("bash")
+        bash("./switch-git-urls")
         git = which("git")
         git('submodule','init')
         git('submodule','update')
 
-        # configure("--prefix=%s" % prefix)
-        # make()
-        # make("install")
+        cfg = "wcb -v -v"
+        cfg += " --prefix=%s" % prefix
+        cfg += " --boost-mt"
+        cfg += " --boost-libs=%s/lib --boost-includes=%s/include" % \
+               (spec["boost"].prefix, spec["boost"].prefix)
+        cfg += " --with-root=%s" % spec["root"].prefix
+        cfg += " configure"
+        python(*cfg.split())
+        python("wcb")
+        python("wcb", "install")
         return
