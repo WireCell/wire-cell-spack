85a7449102ce4a5652cdae578d6cf5459f52339b
Brett Viren <brett.viren@gmail.com>
2017-11-14 09:51:17 -0500

    Sync up a bit with the version in Spack proper

--- repo/packages/root/package.py
+++ repo/packages/root/package.py
@@ -7,6 +7,8 @@ class Root(Package):
     homepage = "https://root.cern.ch"
     url      = "https://root.cern.ch/download/root_v6.08.00.source.tar.gz"
 
+    version('6.10.08', '48f5044e9588d94fb2d79389e48e1d73', preferred=True)
+    version('6.08.06', 'bcf0be2df31a317d25694ad2736df268')
     version('6.08.00', '8462a530d27fa5ca7718ea4437632c3c')
     version('6.07.06', '1180254be7ece0f16142b14381b22d68')
     version('6.07.02', '3fb585bf9fa6ce06ca503173c8bee107')
@@ -14,54 +16,49 @@ class Root(Package):
 
     if sys.platform == 'darwin': patch('math_uint.patch')
 
-    depends_on("cmake")
+    depends_on('cmake@3.4.3:', type='build')
+    depends_on('pkg-config', type='build')
+    depends_on('zlib')
+    depends_on("freetype")
     depends_on("pcre")
+    depends_on('xz')
+
     depends_on("fftw")
 
     depends_on("graphviz",when="+graphviz")
-    depends_on("python")
+    depends_on("python@2.7:")   # FNAL holds us back
     depends_on("gsl")
     depends_on("libxml2+python")
     depends_on("jpeg")
-    depends_on("openssl@1.0.2j")
-    depends_on("freetype")
-    if sys.platform != 'darwin': 
-        depends_on("libpng")
+    depends_on("sqlite")
+    depends_on("openssl@1.0.2k")
 
-    def install(self, spec, prefix):
+    depends_on("libpng")
 
-
-        build_directory = join_path(self.stage.path, 'spack-build')
-        source_directory = self.stage.source_path
-
-        options=[source_directory]
-        if '+debug' in spec:
-            options.append('-DCMAKE_BUILD_TYPE:STRING=Debug')
-        else:
-            options.append('-DCMAKE_BUILD_TYPE:STRING=Release')
-        
-        options.extend(std_cmake_args)
-
-        if sys.platform == 'darwin': 
-            darwin_options= [
-            '-Dcastor=OFF',
-            '-Drfio=OFF',
-            '-Dbonjour=OFF',
+    def cmake_args(self):
+        args = [
             '-Dcocoa=OFF',
+            '-Dbonjour=OFF',
             '-Dx11=ON',
-            '-Ddcache=OFF' ]
-            options.extend(darwin_options)
-        options.extend(['-Dminuit2=ON'])
+            '-Dopengl=ON',
+            '-Dbuiltin_fftw3=OFF',
+            '-Dcxx14=ON',
+        ]
 
-        def setup_dependent_environment(self, module, spec, dep_spec):
-           """Root wants to set ROOTSYS"""
-           os.environ['ROOTSYS'] = self.prefix
- 
+        if sys.platform == 'darwin':
+            args.extend([
+                '-Dcastor=OFF',
+                '-Drfio=OFF',
+                '-Ddcache=OFF',
+            ])
+            
+            return args
+                                    
 
-        with working_dir(build_directory, create=True):
-            cmake(*options)
-            make()
-            make("install")
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        spack_env.set('ROOTSYS', self.prefix)
+        spack_env.set('ROOT_VERSION', 'v{0}'.format(self.version.up_to(1)))
+        spack_env.prepend_path('PYTHONPATH', self.prefix.lib)
 
     def url_for_version(self, version):
         """Handle ROOT's unusual version string."""
