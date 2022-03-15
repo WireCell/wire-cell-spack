2a34e72ba931057a85ee47ca70b76199824560d6
Brett Viren <brett.viren@gmail.com>
2022-03-15 14:19:52 -0400

    Wholesale refresh.

---
+++ packages/go-jsonnet/package.py
@@ -0,0 +1,78 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install go-jsonnet
+#
+# You can edit this file again by typing:
+#
+#     spack edit go-jsonnet
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class GoJsonnet(Package):
+    """Implementation of Jsonnet in pure Go"""
+
+    homepage = "https://github.com/google/go-jsonnet"
+    url      = "https://github.com/google/go-jsonnet/archive/refs/tags/v0.18.0.tar.gz"
+
+    maintainers = ['brettviren']
+
+    version('0.18.0', sha256='369af561550ba8cff5dd7dd08a771805a38d795da3285221012cf3a2933b363e')
+    version('0.17.0', sha256='4fd04d0c9e38572ef388d28ea6b1ac151b8a9a5026ff94e3a68bdbc18c4db38a')
+
+    depends_on('go', type='build')
+    depends_on('git', type='build')
+    depends_on('python', when='+python', type=('build', 'run'))
+
+    variant("python", default=False,
+            description="Provide Python bindings for go-jsonnet")
+    extends("python", when="+python")
+    depends_on("py-setuptools", type=("build",), when="+python")
+    depends_on("py-pip", type=("build",), when="+python")
+    depends_on("py-wheel", type=("build",), when="+python")
+
+    def install(self, spec, prefix):
+        # env['GOPATH'] = self.stage.source_path
+        env['CGO_CXXFLAGS'] = "-std=c++17 -Wall"
+        mkdirp(prefix.bin)
+        mkdirp(prefix.lib)
+        mkdirp(prefix.include)
+        git = which('git')
+        git('clone','-b','v'+str(spec.version),
+            'https://github.com/google/jsonnet.git', 'cpp-jsonnet')
+        go = which('go')
+        go('build', '-o', prefix.bin, './cmd/jsonnet')
+        go('build', '-o', prefix.bin, './cmd/jsonnetfmt')
+        go('build', '-o', prefix.bin, './cmd/jsonnet-deps')
+        go('build', '-o', 'libgojsonnet.so',
+           '-buildmode=c-shared', './c-bindings')
+        # install("jsonnet", prefix.bin)
+        # install("jsonnetfmt", prefix.bin)
+        # install("jsonnet-deps", prefix.bin)
+        install("libgojsonnet.so", prefix.lib)
+        install("libgojsonnet.h", prefix.include)
+        install("libgojsonnet.so", prefix.lib.join("libjsonnet.so"))
+        install("libgojsonnet.h", prefix.include.join("libjsonnet.h"))
+
+
+    @run_after("install")
+    def python_install(self):
+        if "+python" in self.spec:
+            args = list(std_pip_args)
+            args.append("--prefix=" + self.prefix)
+            args.append(".")
+            pip(*args)
---
+++ packages/wire-cell-toolkit/package.py
@@ -0,0 +1,56 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class WireCellToolkit(Package):
+    """Toolkit for Liquid Argon TPC Reconstruction and Visualization ."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://wirecell.github.io/"
+    url      = "https://github.com/WireCell/wire-cell-toolkit/archive/refs/tags/0.17.1.tar.gz"
+
+    maintainers = ['brettviren']
+
+    version('0.18.0', sha256='9a659d2ac96cff0166c1e78574734981ed1fabe039a2f02376a8b3f04653b6b3')
+    version('0.17.1', sha256='4abb1bf16a59815e1702c6c53238c6790ec0872a58f51fd2fc44866419b597e5')
+
+    # optional, default=True
+    variant('tbb', default=True,
+            description='Support multithread execution')
+
+    # optional, default=False
+    variant('cppjsonnet', default=False,
+            description='Use slower C++ Jsonnet instead of Go Jsonnet')
+    variant('hdf', default=False,
+            description='Add support for HDF5')
+    variant('root', default=False,
+            description='Add support for ROOT')    
+
+
+    # just for build time (for waf/wcb)
+    depends_on('python', type=('build',)) 
+
+    # required
+    depends_on('eigen @3.3.0:')
+    depends_on('spdlog @1.9.1:')
+    depends_on('fftw @3.3.9: ~mpi')
+    depends_on('jsoncpp @1.9.3: cxxstd=17')
+    depends_on('boost @1.77.0: cxxstd=17')
+
+    # we need one or the other
+    depends_on('jsonnet @0.17.0:', when='+cppjsonnet')
+    depends_on('go-jsonnet @0.17.0:', when='~cppjsonnet')
+
+    # optional
+    depends_on('intel-tbb @2021.1.1: cxxstd=17', when='+tbb')
+    # fixme: may need to tell root to use same TBB
+    depends_on('root cxxstd=17', when='+root')
+    depends_on('h5cpp', when='+hdf')
+
+    def install(self, spec, prefix):
+        cfg = ["./wcb", "configure", "--prefix={0}".format(prefix)]
+        shell(cfg)
