42cb0d37e23b87bd6821140035f557a2d38c97c6
Patrick Gartung <gartung@fnal.gov>
2025-07-18 09:36:47 -0500

    Add pip std args from PythonPipBuilder

--- spack_repo/wirecell/packages/jsonnet/package.py
+++ spack_repo/wirecell/packages/jsonnet/package.py
@@ -5,10 +5,11 @@
 
 
 from spack_repo.builtin.build_systems.makefile import MakefilePackage
+from spack_repo.builtin.build_systems.python import PythonExtension, PythonPipBuilder
 from spack.package import *
 
 
-class Jsonnet(MakefilePackage):
+class Jsonnet(MakefilePackage, PythonExtension):
     """A data templating language for app and tool developers based on JSON"""
 
     homepage = "https://jsonnet.org/"
@@ -38,5 +39,6 @@ class Jsonnet(MakefilePackage):
     @run_after("install")
     def python_install(self):
         if "+python" in self.spec:
-            args = std_pip_args + ["--prefix=" + self.prefix, "."]
+            pip_args = PythonPipBuilder.std_args(self)
+            args = list(pip_args) + ["--prefix=" + self.prefix, "."]
             pip(*args)
